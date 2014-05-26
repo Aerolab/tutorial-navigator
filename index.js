6a3e860e23d3b4b9c4c5f3df54d63f2ade7f3447
Cristian Douce <cristian@gravityonmars.com>
2014-05-26 13:26:16 -0300

    Improve TutorialView

--- index.js
+++ index.js
@@ -3,24 +3,9 @@
  */
 
 var TutorialView = require('tutorial-view');
-var nativeapps = require('native-platforms');
-var clientapps = require('client-platforms');
-var serverapis = require('server-apis');
-var serverapps = require('server-platforms');
 var dom = require('dom');
 
-var tutorial = {
-  apptype: '',
-  nativePlatform: '',
-  clientPlatform: '',
-  serverApi: '',
-  serverPlatform: '',
-  nativeapps: nativeapps,
-  clientapps: clientapps,
-  serverapis: serverapis,
-  serverapps: serverapps
-}
 
-var view = new TutorialView(tutorial);
+var view = new TutorialView();
 
 dom('section.content').append(view.render());
